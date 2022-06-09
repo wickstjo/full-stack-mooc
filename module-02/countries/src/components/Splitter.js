f3ab37444529f4f700be868a4a96228bd27399aa
wickstjo <jfwick@gmail.com>
2022-06-09 23:54:59 +0300

    removing node_modules

--- module-02/countries/src/components/Splitter.js
+++
@@ -1,8 +0,0 @@
-const Splitter = ({ first, second }) => { return (
-    <div className={ 'splitter' }>
-        <div>{ first }</div>
-        <div>{ second }</div>
-    </div>
-)}
-
-export default Splitter;
--- module-03/json-api/json-server.sh
+++
@@ -1,1 +0,0 @@
-npx json-server --port 3001 --watch db.json
